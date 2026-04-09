b113c96dcc092ecc11f19216c4197a4af3b7ee30
dngphuc05 <dngphuc05@users.noreply.github.com>
2026-04-09 13:29:00 +0700

    scaffold: add MainActivity scaffold block 001

---
+++ app/src/main/java/com/pocketmocap/app/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.pocketmocap.app
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import org.json.JSONObject
+
