90687eb416eb30aebb77749c6cb67ef6dd81b991
dngphuc05 <dngphuc05@users.noreply.github.com>
2026-04-09 10:49:00 +0700

    scaffold: add build-gradle configuration block 012

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,3 +103,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
