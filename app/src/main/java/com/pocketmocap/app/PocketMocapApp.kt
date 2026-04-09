af4105f5264d7ba0f9b90b5588ae136f2722c357
dngphuc05 <dngphuc05@users.noreply.github.com>
2026-04-09 14:16:00 +0700

    scaffold: add PocketMocapApp scaffold block 001

---
+++ app/src/main/java/com/pocketmocap/app/PocketMocapApp.kt
@@ -0,0 +1,18 @@
+package com.pocketmocap.app
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.pocketmocap.app.PocketMocapViewModel.ConnectionState
