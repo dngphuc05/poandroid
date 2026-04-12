b27578decf47be127e64f746f8e48a258ad6cbac
dngphuc05 <dngphuc05@users.noreply.github.com>
2026-04-12 16:26:00 +0700

    scaffold: add gradlew scaffold block 006

--- gradlew.bat
+++ gradlew.bat
@@ -65,3 +65,16 @@ echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
+goto fail
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=
+
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
+
+:end
+@rem End local scope for the variables with windows NT shell
