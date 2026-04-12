9f0eb18db802c86481ca329d41f64b73770d4c97
dngphuc05 <dngphuc05@users.noreply.github.com>
2026-04-12 21:55:00 +0700

    scaffold: add settings-gradle configuration block 002

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,11 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
