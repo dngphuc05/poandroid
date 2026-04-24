f48e18ff98e1a9f320a802d90baa6e10051098d4
dngphuc05 <dngphuc05@users.noreply.github.com>
2026-04-24 09:59:00 +0700

    uiscreens: add SkeletonSurfaceView screen state block 001

---
+++ app/src/main/java/com/pocketmocap/app/ui/SkeletonSurfaceView.kt
@@ -0,0 +1,14 @@
+package com.pocketmocap.app.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.DashPathEffect
+import android.graphics.Paint
+import android.graphics.PixelFormat
+import android.graphics.PorterDuff
+import android.graphics.RectF
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import kotlin.math.max
+
