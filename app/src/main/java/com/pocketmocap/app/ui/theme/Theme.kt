ced50fe3acb23b57ae25e0b6c5233265eaf82435
dngphuc05 <dngphuc05@users.noreply.github.com>
2026-04-25 22:26:00 +0700

    uiscreens: add Theme screen state block 001

---
+++ app/src/main/java/com/pocketmocap/app/ui/theme/Theme.kt
@@ -0,0 +1,18 @@
+package com.pocketmocap.app.ui.theme
+
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.FiniteAnimationSpec
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
